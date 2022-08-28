1ade5aceba5f3f283ef2400865afff4bfe2d9d4d
itsmarbo <marlenne.bf@gmail.com>
2022-08-28 23:43:43 +0200

    New sequences

--- JS/action.js
+++ JS/action.js
@@ -3,7 +3,7 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            endpoint: "http://itsmarbo.pythonanywhere.com/",
+            endpoint: "",
             dnaSequence: ["A"],
             aaSequence: ["G"],
             dnaPostSequence: ["A"],
